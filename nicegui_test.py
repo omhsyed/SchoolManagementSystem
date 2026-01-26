7b0e82421ce9a9ebde6a5b0c21717cea982ae87e
omhsyed <omhsyed@gmail.com>
2026-01-25 21:21:26 -0600

    working with ui layout and subpages

--- nicegui_test.py
+++ nicegui_test.py
@@ -11,27 +11,7 @@ username_input = ''
 password_input = ''
 schoolcode_input = ''
 
-bg = "#8688A1"
-
-
-@ui.page('/')
-def index():
-
-    ui.query('body').style(f'background-color: {bg}')
-    ui.colors(primary = "#FFFFFF")
-
-    with ui.row():
-        ui.input(label='Username', 
-                #placeholder = 'start typing', 
-                on_change = lambda e: read_username(e.value), 
-                #validation = {'Input too long': lambda value: len(value) < 20}
-                ).props('rounded outlined dense').props('clearable')
-
-        ui.input(label='Password', 
-                #placeholder='start typing', 
-                on_change = lambda e: read_password(e.value),
-                #validation={'Input too long': lambda value: len(value) < 20}
-                ).props('rounded outlined dense').props('clearable')
+bg = "#51785A"
 
 
 def read_username(value):
@@ -43,12 +23,56 @@ def read_password(value):
      global password_input
      password_input = value
 
+def login():
+     if username_input == "test1" and password_input == "test2":
+          ui.label("it worked!")
+          ui.navigate.to('/other')
 
 
-connection = mysql.connector.connect(host = "localhost", user = "root", password = os.environ["DB_PASSWORD"])
+with ui.header().style('background-color: #3874c8').classes("justify-center"):
+     ui.label("Al-Falah Classroom").classes("justify-center")
 
-if connection.is_connected():
-     cursor = connection.cursor()
+
+ui.query('body').style(f'background-color: {bg}')
+ui.colors(primary = "#FBFFCA", secondary = "#FBFFCA", accent = "#FBFFCA")
+
+
+#@ui.page('/')
+def login_page():
+
+     
+     ui.input(label='Username', 
+               #placeholder = 'start typing', 
+               on_change = lambda e: read_username(e.value), 
+               #validation = {'Input too long': lambda value: len(value) < 20}
+               ).props('rounded outlined dense').props('clearable')
+     
+
+     ui.input(label='Password', 
+               #placeholder='start typing', 
+               on_change = lambda e: read_password(e.value),
+               #validation={'Input too long': lambda value: len(value) < 20}
+               ).props('rounded outlined dense').props('clearable')
+
+     
+     login_button = ui.button(color = "#FBFFCA", text = "Log In", on_click = login)
+     
+
+
+
+def other():
+
+     ui.label("different page!")
+     ui.button(text = "go back", on_click = lambda e: ui.navigate.to('/'))
+
+
+ui.sub_pages({'/': login_page, '/other': other}).classes('w-full items-center')
+
+
+#connection = mysql.connector.connect(host = "localhost", user = "root", password = os.environ["DB_PASSWORD"])
+
+#if connection.is_connected():
+     #cursor = connection.cursor()
 
 
 
