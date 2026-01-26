98a98739610a1c6f3b5aafd75b93e68ca5e12ad8
omhsyed <omhsyed@gmail.com>
2026-01-25 21:48:13 -0600

    more aesthetic changes + error message

--- nicegui_test.py
+++ nicegui_test.py
@@ -1,17 +1,22 @@
 from nicegui import app, ui
 import mysql.connector
-#from dotenv import load_dotenv
 import os
 
 
-#load_dotenv()
 
+
+# Variables
 
 username_input = ''
 password_input = ''
 schoolcode_input = ''
+clr_bg = "#53745B"
+clr_accent = "#2D4232"
+clr_text = "#FCFFE8"
+clr_button = "#C8DFAC"
+clr_error = "#511E0B"
 
-bg = "#51785A"
+
 
 
 def read_username(value):
@@ -27,19 +32,26 @@ def login():
      if username_input == "test1" and password_input == "test2":
           ui.label("it worked!")
           ui.navigate.to('/other')
+     else:
+          ui.html(f"<h7 style = 'color: {clr_error};'> Incorrect Log-in Credentials. </h7>", sanitize = False)
 
 
-with ui.header().style('background-color: #3874c8').classes("justify-center"):
-     ui.label("Al-Falah Classroom").classes("justify-center")
 
 
-ui.query('body').style(f'background-color: {bg}')
+with ui.header().style(f'background-color: {clr_accent}').classes("justify-center"):
+     ui.html(f"<h4 style = 'color: {clr_text};'> <strong> Al-Falah Classroom </strong> </h4>", sanitize = False)
+
+
+
+ui.query('body').style(f'background-color: {clr_bg}')
 ui.colors(primary = "#FBFFCA", secondary = "#FBFFCA", accent = "#FBFFCA")
 
 
-#@ui.page('/')
+
+
 def login_page():
 
+     ui.html(f"<h6 style = 'color: {clr_text};'> Welcome to Al-Falah Academy's <strong>School System.</strong> </h6>", sanitize = False)
      
      ui.input(label='Username', 
                #placeholder = 'start typing', 
@@ -55,15 +67,15 @@ def login_page():
                ).props('rounded outlined dense').props('clearable')
 
      
-     login_button = ui.button(color = "#FBFFCA", text = "Log In", on_click = login)
+     login_button = ui.button(color = clr_button, text = "Log In", on_click = login)
      
 
 
 
 def other():
 
-     ui.label("different page!")
-     ui.button(text = "go back", on_click = lambda e: ui.navigate.to('/'))
+     ui.html(f"<h7 style = 'color: {clr_text};'> Page Two!!! </h7>", sanitize = False)
+     ui.button(color = clr_button, text = "go back", on_click = lambda e: ui.navigate.to('/'))
 
 
 ui.sub_pages({'/': login_page, '/other': other}).classes('w-full items-center')
