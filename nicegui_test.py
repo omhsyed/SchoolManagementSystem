e8916f0e66b369eb2d8043f8222462f05ece0636
Omer Syed <omhsyed@gmail.com>
2026-01-25 18:05:37 -0600

    Add files via upload

---
+++ nicegui_test.py
@@ -0,0 +1,36 @@
+from nicegui import app, ui
+
+username_input = ''
+
+bg = "#8688A1"
+
+@ui.page('/')
+def index():
+
+    ui.query('body').style(f'background-color: {bg}')
+    ui.colors(primary = "#FFFFFF")
+
+    with ui.row():
+        ui.input(label='Username', 
+                #placeholder = 'start typing', 
+                on_change = lambda e: read_username(e.value), 
+                #validation = {'Input too long': lambda value: len(value) < 20}
+                ).props('rounded outlined dense').props('clearable')
+
+        ui.input(label='Password', 
+                #placeholder='start typing', 
+                #validation={'Input too long': lambda value: len(value) < 20}
+                ).props('rounded outlined dense').props('clearable')
+
+
+    def read_username(value):
+        global username_input
+        username_input = value
+
+
+
+ui.run(title='Test site', 
+       #native = True, 
+       #window_size = (800, 600), 
+       #fullscreen = False
+       )
