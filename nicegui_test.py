237d2042395556e166cf50e19ff659e198b9eb45
omhsyed <omhsyed@gmail.com>
2026-01-25 19:35:02 -0600

    working with sql connection credentials, will likely use os environment variable set in a web deployer like replit

--- nicegui_test.py
+++ nicegui_test.py
@@ -1,6 +1,15 @@
 from nicegui import app, ui
+import mysql.connector
+#from dotenv import load_dotenv
+import os
+
+
+#load_dotenv()
+
 
 username_input = ''
+password_input = ''
+schoolcode_input = ''
 
 bg = "#8688A1"
 
@@ -20,13 +29,29 @@ def index():
 
         ui.input(label='Password', 
                 #placeholder='start typing', 
+                on_change = lambda e: read_password(e.value),
                 #validation={'Input too long': lambda value: len(value) < 20}
                 ).props('rounded outlined dense').props('clearable')
 
 
-    def read_username(value):
-        global username_input
-        username_input = value
+def read_username(value):
+     global username_input
+     username_input = value
+
+
+def read_password(value):
+     global password_input
+     password_input = value
+
+
+
+connection = mysql.connector.connect(host = "localhost", user = "root", password = os.environ["DB_PASSWORD"])
+
+if connection.is_connected():
+     cursor = connection.cursor()
+
+
+
 
 
 
